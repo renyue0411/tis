ccb91a09518953f154886f05b1fc9e117162cade
renyue0411 <renyue0411@gmail.com>
2023-12-19 21:39:36 +0900

    init

---
+++ MYSTREAMING/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+// BUFFERSIZE is the size of max packet size
+const BUFFER_SIZE = 1024
+
+const MAX_FRAMES = 50000
+
+// IP PORT: the default ip and port for communication
+//const IP = "192.168.22.2"
+const IP = "0.0.0.0"
+const PORT = "6666"
